f6b937d34eb6cdbe6fff7eb934089c5ee9fc7184
Huy T. Tran <huytran1@illinois.edu>
2022-04-08 16:47:34 -0500

    static site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "We are awarded funding from DARPA!",
+        "excerpt":"Our project titled “A Demonstration Platform for Dynamic Mission Planning with Multi-domain Autonomous Systems” is selected for funding by the DARPA STO program! Our project aims to develop a demonstration platform for teams of quad-rotors and ground-based robots competing in games of capture-the-flag, along with hierarchical reinforcement learning (RL) methods...","categories": ["news"],
+        "tags": ["artificial intelligence","multi-agent systems","reinforcement learning","robotics"],
+        "url": "/news/DARPA/",
+        "teaser": null
+      },{
+        "title": "Our work on social sensing was accepted to Sustainable and Resilient Infrastructure!",
+        "excerpt":"Our paper “Social sensing: towards social media as a sensor for resilience in power systems and other critical infrastructures” was accepted for publication in Sustainable and Resilient Infrastructure! We demonstrate the value of social media for critical infrastructure (CI) resilience by using social media to model CI behaviors at higher...","categories": ["news"],
+        "tags": ["data science","resilient systems"],
+        "url": "/news/SRI/",
+        "teaser": null
+      },{
+        "title": "Our work on hierarchical RL was accepted to ICRA 2020!",
+        "excerpt":"Our paper “Evaluating Adaptation Performance of Hierarchical Deep Reinforcement Learning” was accepted at the 2020 IEEE International Conference on Robotics and Autonomation (ICRA)! We show that hierarchical RL better adapts to new enemy dynamics in a game of capture-the-flag than typical “flat” RL architectures.  ","categories": ["news"],
+        "tags": ["multi-agent systems","reinforcement learning","robotics"],
+        "url": "/news/icra/",
+        "teaser": null
+      },{
+        "title": "We are awarded funding from the ONR Science of AI program!",
+        "excerpt":"Our project titled “Explainable AI for Mission Planning and Execution with Interpretable Courses of Action” is selected for funding by the ONR Science of AI program! Navy mission planning and execution requires the ability to consider a large number of potential courses of action (COAs) and ensure optimal decision-making in...","categories": ["news"],
+        "tags": ["artificial intelligence","controls","multi-agent systems","reinforcement learning","robotics"],
+        "url": "/news/ONR/",
+        "teaser": null
+      },{
+        "title": "Our work on airline network resilience was accepted to Transportation Research Part E!",
+        "excerpt":"Our paper “Data-driven Analysis of Resilience in Airline Networks” was accepted for publication in Transportation Research Part E: Logistics and Transportation Review! We present new techniques combining data-driven methods with network theory to analyze resilience in airline networks. We find that simultaneously considering data-driven and network approaches provides a more...","categories": ["news"],
+        "tags": ["data science","intelligent transportation systems","network theory","resilient systems"],
+        "url": "/news/TRE/",
+        "teaser": null
+      },{
+        "title": "Our work on network robustness was accepted to the IEEE Systems Journal!",
+        "excerpt":"Our paper “A Complex Network Framework for Validated Assessments of Systems of Systems Robustness” was accepted for publication in the IEEE Systems Journal! We present a framework for rapid quantitative comparisons of robustness in system of systems (SoS) architectures that leverages complex network methods for assessing robustness and design of...","categories": ["news"],
+        "tags": ["network theory","systems engineering"],
+        "url": "/news/ieee-systems/",
+        "teaser": null
+      },{
+        "title": "We are awarded funding as a project PI on the ONR ALERT program for undergraduate research!",
+        "excerpt":"We are excited to be part of the UIUC team (as a project PI) awarded funding from ONR for the Accelerated Learning and Engineering Research Training (ALERT) Program in Cyber and Electronic Warfare for Undergraduate Midshipmen and Cadets. This award gives us the opportunity to work with talented undergraduate ROTC...","categories": ["news"],
+        "tags": ["machine learning"],
+        "url": "/news/alert/",
+        "teaser": null
+      },{
+        "title": "We are awarded funding from the ARL A2I2 program!",
+        "excerpt":"Our project titled “Robust and Adaptive Autonomy for Multi-agent Maneuvers (RAAMM)” is selected for funding by the ARL A2I2 program! AI/ML systems for Army missions require the ability to adapt to changing dynamics and adversaries. This project aims to address this critical challenge through three research thrusts. Thrust one will...","categories": ["news"],
+        "tags": ["artificial intelligence","machine learning","multi-agent systems","reinforcement learning","robotics"],
+        "url": "/news/ARL/",
+        "teaser": null
+      },{
+        "title": "Our work on SFs for MARL was accepted to AAMAS 2022!",
+        "excerpt":"Our paper “Disentangling Successor Features for Coordination in Multi-agent Reinforcement Learning” was accepted for publication as a full paper and oral presentation at AAMAS 2022! We show that our approach for multi-agent reinforcement learning (MARL), named DISentangling Successor features for Coordination (DISSC), improves performance and training time relative to existing...","categories": ["news"],
+        "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
+        "url": "/news/aamas-dissc/",
+        "teaser": null
+      },{
+        "title": "Our work on hierarchical RL was accepted to ALA Workshop 2022!",
+        "excerpt":"Our paper “Feature Specialization and Clustering Improves Hierarchical Subtask Learning” was accepted for presentation at the Adaptive Learning Agents (ALA) Workshop 2022 (AAMAS)! We show that feature specialization and clustering improve hierarchical reinforcement learning (RL) in various spatial planning tasks, through our Specialized Neurons and Clustering (SNAC) method.  ","categories": ["news"],
+        "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
+        "url": "/news/aamas-snac/",
+        "teaser": null
+      },{
+        "title": "Assured Autonomy",
+        "excerpt":"We work on artificial intelligence and formal methods to assure safety and performance from autonomous systems.   Explaining RL agents   This work leverages formal methods to infer human-interpretable explanations of data-driven autonomous agents, such as those trained using deep reinforcement learning.  ","categories": [],
+        "tags": ["formal methods","reinforcement learning"],
+        "url": "/research/assured-autonomy/",
+        "teaser": "/assets/images/ltl.png"
+      },{
+        "title": "Autonomous Flight",
+        "excerpt":"We work on artificial intelligence and controls methods to enable autonomous flight vehicles, for example for space applications and urban air mobility. Fast adaptation for autonomous landers This work explores the use of model-based meta-RL for designing a navigation and control system for autonomous landing of a space vehicles. The...","categories": [],
+        "tags": ["controls","reinforcement learning"],
+        "url": "/research/autonomous-flight/",
+        "teaser": "/assets/images/lunar-lander.jpeg"
+      },{
+        "title": "Hierarchical RL",
+        "excerpt":"We work on hierarchical reinforcement learning (RL) methods to create autonomous agents that can efficiently solve complex tasks. Bioinspired discovery of hierarchical subtasks Value maps for two learned sub-policies. The object-based option is learned using features specialized to focus on dynamic objects - it appears to have the blue agent...","categories": [],
+        "tags": ["artificial intelligence","reinforcement learning"],
+        "url": "/research/hrl/",
+        "teaser": "/assets/images/hrl.png"
+      },{
+        "title": "Human-machine Teaming",
+        "excerpt":"We work on learning methods to create autonomous agents that can effectively partner with different humans in collaborative tasks, like search-and-rescue, space exploration, and autonomous construction. Learning adaptable human models with successor features This work explores the use of successor features to learn decision-making models of humans that can be...","categories": [],
+        "tags": ["artificial intelligence","human-machine teaming","multi-agent systems","reinforcement learning"],
+        "url": "/research/hum-t/",
+        "teaser": "/assets/images/hum-t.png"
+      },{
+        "title": "Intelligent Transportation Systems",
+        "excerpt":"We work on artificial intelligence and machine learning methods to improve autonomy and traffic management in intelligent transportation systems (ITS). MARL for traffic light control This work explores applications of MARL to traffic light control. We are specifically exploring the integration of hierarchical RL with MARL for this problem. We...","categories": [],
+        "tags": ["intelligent transportation systems","reinforcement learning"],
+        "url": "/research/its/",
+        "teaser": "/assets/images/its.jpeg"
+      },{
+        "title": "Multi-agent RL",
+        "excerpt":"We work on multi-agent reinforcement learning (MARL) methods to create teams of autonomous agents that can work together to solve challenging tasks, such as search-and-rescue, traffic control, autonomous driving, and warehouse robotics. Improving decentralized training with successor features This work leverages successor features (SFs) to disentangle an individual agent’s impact...","categories": [],
+        "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
+        "url": "/research/marl/",
+        "teaser": "/assets/images/dissc.png"
+      }]
