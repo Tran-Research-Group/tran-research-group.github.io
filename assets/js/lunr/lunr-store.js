2f8c0eb8ec623717cca4a215a2b237f26acd2fad
Huy T. Tran <huytran1@illinois.edu>
2023-09-19 09:40:53 -0500

    update site files

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,37 +2,37 @@ var store = [{
         "title": "We are awarded funding from DARPA!",
         "excerpt":"Our project titled “A Demonstration Platform for Dynamic Mission Planning with Multi-domain Autonomous Systems” is selected for funding by the DARPA STO program! Our project aims to develop a demonstration platform for teams of quad-rotors and ground-based robots competing in games of capture-the-flag, along with hierarchical reinforcement learning (RL) methods...","categories": ["news"],
         "tags": ["artificial intelligence","multi-agent systems","reinforcement learning","robotics"],
-        "url": "/news/DARPA/",
+        "url": "/news/DARPA-demo/",
         "teaser": null
       },{
         "title": "Our work on social sensing was accepted to Sustainable and Resilient Infrastructure!",
         "excerpt":"Our paper “Social sensing: towards social media as a sensor for resilience in power systems and other critical infrastructures” was accepted for publication in Sustainable and Resilient Infrastructure! We demonstrate the value of social media for critical infrastructure (CI) resilience by using social media to model CI behaviors at higher...","categories": ["news"],
         "tags": ["data science","resilient systems"],
-        "url": "/news/SRI/",
+        "url": "/news/SRI-sm/",
         "teaser": null
       },{
         "title": "Our work on hierarchical RL was accepted to ICRA 2020!",
         "excerpt":"Our paper “Evaluating Adaptation Performance of Hierarchical Deep Reinforcement Learning” was accepted at the 2020 IEEE International Conference on Robotics and Autonomation (ICRA)! We show that hierarchical RL better adapts to new enemy dynamics in a game of capture-the-flag than typical “flat” RL architectures.  ","categories": ["news"],
         "tags": ["multi-agent systems","reinforcement learning","robotics"],
-        "url": "/news/icra/",
+        "url": "/news/icra-hrl/",
         "teaser": null
       },{
         "title": "We are awarded funding from the ONR Science of AI program!",
         "excerpt":"Our project titled “Explainable AI for Mission Planning and Execution with Interpretable Courses of Action” is selected for funding by the ONR Science of AI program! Navy mission planning and execution requires the ability to consider a large number of potential courses of action (COAs) and ensure optimal decision-making in...","categories": ["news"],
         "tags": ["artificial intelligence","controls","multi-agent systems","reinforcement learning","robotics"],
-        "url": "/news/ONR/",
+        "url": "/news/ONR-xai/",
         "teaser": null
       },{
         "title": "Our work on airline network resilience was accepted to Transportation Research Part E!",
         "excerpt":"Our paper “Data-driven Analysis of Resilience in Airline Networks” was accepted for publication in Transportation Research Part E: Logistics and Transportation Review! We present new techniques combining data-driven methods with network theory to analyze resilience in airline networks. We find that simultaneously considering data-driven and network approaches provides a more...","categories": ["news"],
         "tags": ["data science","intelligent transportation systems","network theory","resilient systems"],
-        "url": "/news/TRE/",
+        "url": "/news/TRE-atn/",
         "teaser": null
       },{
         "title": "Our work on network robustness was accepted to the IEEE Systems Journal!",
         "excerpt":"Our paper “A Complex Network Framework for Validated Assessments of Systems of Systems Robustness” was accepted for publication in the IEEE Systems Journal! We present a framework for rapid quantitative comparisons of robustness in system of systems (SoS) architectures that leverages complex network methods for assessing robustness and design of...","categories": ["news"],
         "tags": ["network theory","systems engineering"],
-        "url": "/news/ieee-systems/",
+        "url": "/news/ieee-systems-sos/",
         "teaser": null
       },{
         "title": "We are awarded funding as a project PI on the ONR ALERT program for undergraduate research!",
@@ -44,7 +44,7 @@ var store = [{
         "title": "We are awarded funding from the ARL A2I2 program!",
         "excerpt":"Our project titled “Robust and Adaptive Autonomy for Multi-agent Maneuvers (RAAMM)” is selected for funding by the ARL A2I2 program! AI/ML systems for Army missions require the ability to adapt to changing dynamics and adversaries. This project aims to address this critical challenge through three research thrusts. Thrust one will...","categories": ["news"],
         "tags": ["artificial intelligence","machine learning","multi-agent systems","reinforcement learning","robotics"],
-        "url": "/news/ARL/",
+        "url": "/news/ARL-raamm/",
         "teaser": null
       },{
         "title": "Our work on SFs for MARL was accepted to AAMAS 2022!",
@@ -74,13 +74,19 @@ var store = [{
         "title": "Huy gave an invited workshop talk at IROS 2022!",
         "excerpt":"Huy gave an invited talk at the IROS 2022 Decision Making in Multi-agent Systems Workshop. The talk was titled “On Utilities for Cooperative Multi-agent RL.” A video link to the talk can be found here.  ","categories": ["news"],
         "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
-        "url": "/news/iros/",
+        "url": "/news/iros-dmmas/",
         "teaser": null
       },{
         "title": "Huy gave an invited workshop talk at RSS 2023!",
         "excerpt":"Huy gave an invited talk at the RSS 2023 Inference and Decision Making for Autonomous Vehicles Workshop. The talk was titled “Learning for Decision Making in Cooperative Multi-agent Systems.”  ","categories": ["news"],
         "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
-        "url": "/news/rss/",
+        "url": "/news/rss-idmav/",
+        "teaser": null
+      },{
+        "title": "Our work on ad hoc teaming was accepted to the IROS 2023 Multi-Agent Learning Workshop!",
+        "excerpt":"Our paper “Coordination in Ad Hoc Teams with Generalized Policy Improvement” was accepted for a 5 minute lightning talk at the IROS 2023 Multi-Agent Learning Workshop! We show that generalized policy improvement allows an agent to leverage prior training for fast adadptation to new teammates in cooperative tasks.  ","categories": ["news"],
+        "tags": ["artificial intelligence","multi-agent systems","reinforcement learning"],
+        "url": "/news/iros-gsat/",
         "teaser": null
       },{
         "title": "Human-robot Teaming",
